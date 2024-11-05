8ac9d81dbcd2c560bd95ca36ed0a9391d8609006
Om Tomar <omtomar21102003@gmail.com>
2024-11-05 07:57:54 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -31,7 +31,7 @@ router.post("/send/mail", async (req, res, next) => {
   }
   try {
     await sendEmail({
-      email: "merndeveloper4@gmail.com",
+      email: "omtomar21102003@gmail.com",
       subject: "GYM WEBSITE CONTACT",
       message,
       userEmail: email,
